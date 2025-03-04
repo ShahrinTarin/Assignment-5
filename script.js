6a34019f9bc01f04b30edb340d1aab71b6aa08fc
ShahrinTarin <rahmanshahrintarin@gmail.com>
2025-03-05 01:57:43 +0600

    date fixed

---
+++ script.js
@@ -0,0 +1,20 @@
+// for body theme 
+document.getElementById("theme").addEventListener('click', function () {
+    document.getElementById("body").style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+});
+
+// for time 
+const currentDate = new Date();
+const dayName ={
+    weekday: "short"
+}
+const allDayName = {
+    day: "numeric",
+    month: "short",
+    year: "numeric"
+}
+const setDay = currentDate.toLocaleDateString("en-US", dayName);
+const setDate = currentDate.toLocaleDateString("en-US", allDayName);
+document.getElementById('day').textContent = setDay+' ,'
+document.getElementById('time').textContent = setDate.replace(",","")
+
